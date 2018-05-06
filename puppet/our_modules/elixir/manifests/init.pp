c0f70052666f4abf5774bd306646f81c68f3335c
andresgutgon <andresgutgon@gmail.com>
2018-05-06 14:03:54 +0200

    Adding Earlang apt repository

--- puppet/our_modules/elixir/manifests/init.pp
+++ puppet/our_modules/elixir/manifests/init.pp
@@ -2,9 +2,25 @@
 # https://www.erlang-solutions.com/resources/download.html
 #
 class elixir (
-  $version
+  $erlang_repo_name,
+  $erlang_repo_location,
+  $erlang_repo_key,
+  $elixir_version
 ) {
-  package { 'elixir':
-    ensure => $version
+  include ::apt
+  apt::source { $erlang_repo_name:
+    location => $erlang_repo_location,
+    repos    => 'contrib',
+    key      => {
+      'id' => $erlang_repo_key
+    }
+  }
+  -> Class['apt::update']
+  -> package { 'esl-erlang':
+      ensure  => 'latest',
+      require => Apt::Source[$erlang_repo_name]
+  } -> package { 'elixir':
+    ensure  => $elixir_version,
+    require => Apt::Source[$erlang_repo_name]
   }
 }
