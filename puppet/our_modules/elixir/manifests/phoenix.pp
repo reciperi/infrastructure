cf2e01da1fddbaba8fb609263496976b51460838
andresgutgon <andresgutgon@gmail.com>
2019-03-02 13:30:54 +0000

    Check folder for Elixir package does not exists before installing. Less noisy puppet log

--- puppet/our_modules/elixir/manifests/phoenix.pp
+++ puppet/our_modules/elixir/manifests/phoenix.pp
@@ -8,9 +8,9 @@ class elixir::phoenix (String $version, String $user) {
 
   exec { 'install_hex':
     command     => 'mix local.hex --force',
-    returns     => [0, 1],
     path        => $paths,
     user        => $user,
+    unless      => "bash -c \"test -d /home/${user}/.mix/archives/hex-*\"",
     environment => $environment,
   }
   -> elixir::package_uninstall { 'phx_new':
@@ -22,6 +22,7 @@ class elixir::phoenix (String $version, String $user) {
   -> exec { 'phoenix_install':
     command     => "mix archive.install hex phx_new ${version} --force",
     path        => $paths,
+    unless      => "bash -c \"test -d /home/${user}/.mix/archives/phx_new-*\"",
     returns     => [0, 1],
     environment => $environment,
     user        => $user
